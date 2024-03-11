59e57c15dec96bc3785af130c53a6c66b8d11f56
macdonst <simon.macdonald@gmail.com>
2024-03-11 13:26:08 -0400

    Static redirects
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- index.mjs
+++ index.mjs
@@ -3,6 +3,15 @@ import { handler as ssrHandler } from "./dist/server/entry.mjs";
 export async function handler(event) {
   console.log(`🚀 ${event.requestContext.http.method}: ${event.rawPath}`);
 
+  if (event.rawPath.startsWith('/_astro')) {
+    return {
+        statusCode: 301,
+        headers: {
+          Location: `/_static${event.rawPath}`,
+        }
+      };
+  }
+
   return await ssrHandler(
     event,
     { host: (process.env.ARC_SANDBOX ? "http://localhost:3333" : null) },
